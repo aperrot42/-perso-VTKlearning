a4685f2678611e4c9becdb1a9dee09e5101250a9
antonin07130 <antonin07130@gmail.com>
2010-04-26 18:21:15 -0400

    very first basic distance example with ITK classes

---
+++ Distance/Distance.cxx
@@ -0,0 +1,21 @@
+#include <vtkMath.h>
+
+int main(int arc, char argv[])
+{
+  // Create 2 pts
+  double p0[3] = {0.0, 0.0, 0.0};
+  double p1[3] = {1.0, 1.0, 1.0};
+
+  // compute squared distance
+  double dist = vtkMath::Distance2BetweenPoints(p0,p1);
+
+  // console output
+
+  std::cout<< "the distance between P0 : " << p0[0] << p0[1] << p0[2]
+  << " and P1 : " << p1[0] << p1[1] << p1[2]
+  << " is " << dist << std::endl;
+
+
+
+}
+

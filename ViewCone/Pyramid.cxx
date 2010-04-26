fa670d668149a8a692dd04971c04a7cb5e3fd24a
antonin07130 <antonin07130@gmail.com>
2010-04-26 19:14:57 -0400

    starting the creation and visualization of VTK 3D objects

---
+++ ViewCone/Pyramid.cxx
@@ -0,0 +1,35 @@
+#include <vtkSmartPointer.h>
+// object definition
+#include <vtkPoints.h>
+#include <vtkCellArray.h> // why ?
+#include <vtkUnstructuredGrid.h> // say what ????
+#include <vtkPyramid.h>
+// object representation
+#include <vtkDataSetMapper.h>
+#include <vtkActor.h>
+// Rendering
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
+int main(int argc, char* argv[])
+{
+// points ensemble
+vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+float p0[3] = {1., 0., 0.}; // we create a 4 points based pyramid
+float p1[3] = {-1., 0., 0.};
+float p2[3] = {0., 1., 0.};
+float p3[3] = {0., -1., 0.};
+float p4[3] = {0., 0., 1.};
+
+points->InsertNextPoint(p0); // store the points in a points ensemble (points)
+points->InsertNextPoint(p1);
+points->InsertNextPoint(p2);
+points->InsertNextPoint(p3);
+points->InsertNextPoint(p4);
+
+vtkSmartPointer<vtkPyramid> Pyramid = vtkSmartPointer<vtkPyramid>::New();
+
+Pyramid->Contour();
+}
+
